2ee2feaad60f284efd805d3f17ef92270c5d1e80
Christian Hedel <cwasilei@gmail.com>
2024-07-09 21:31:33 +0200

    cargo +nightly fmt

--- src/balances.rs
+++ src/balances.rs
@@ -1,39 +1,33 @@
 use std::collections::BTreeMap;
 
-
 pub struct Pallet {
-  balances: BTreeMap<String, u128>
+    balances: BTreeMap<String, u128>,
 }
-
 
 impl Pallet {
-  pub fn new() -> Self {
-    Self {
-      balances:  BTreeMap::new()
+    pub fn new() -> Self {
+        Self { balances: BTreeMap::new() }
     }
-  }
 
-  /// Set the balance of an account `who` to some `amount`.
-  pub fn set_balance(&mut self, who: &String, amount: u128) {
-    self.balances.insert(who.to_string(), amount);
-  }
+	/// Set the balance of an account `who` to some `amount`.
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
+        self.balances.insert(who.to_string(), amount);
+    }
 
-  /// Get the balance of an account `who`.
-  /// If the account has no stored balance, we return zero.
-  pub fn balance(&self, who: &String) -> u128 {
-    *self.balances.get(who).unwrap_or(&0)
-  }
-
+    /// Get the balance of an account `who`.
+    /// If the account has no stored balance, we return zero.
+    pub fn balance(&self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0)
+    }
 }
-
 
 #[test]
 fn init_balances() {
-  let mut p = Pallet::new();
+    let mut p = Pallet::new();
 
-  assert_eq!(p.balance(&String::from("wasi")), 0);
+    assert_eq!(p.balance(&String::from("wasi")), 0);
 
-  p.set_balance(&String::from("wasi"), 1);
+    p.set_balance(&String::from("wasi"), 1);
 
-  assert_eq!(p.balance(&String::from("wasi")), 1);
+    assert_eq!(p.balance(&String::from("wasi")), 1);
 }
