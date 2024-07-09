67e7c7907bd59cae961bf1b0a7dd4c389e3e70da
Christian Hedel <cwasilei@gmail.com>
2024-07-09 23:54:34 +0200

    make system functional

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
 mod balances;
+mod system;
+
+// This is our main Runtime.
+// It accumulates all of the different pallets we want to use.
+pub struct Runtime {
+    system: system::Pallet,
+    balances: balances::Pallet,
+}
+
+impl Runtime {
+    // Create a new instance of the main Runtime, by creating a new instance of each pallet.
+    fn new() -> Self {
+        Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    /* TODO: Create a mutable variable `runtime`, which is a new instance of `Runtime`. */
+    let mut runtime = Runtime::new();
+    /* TODO: Set the balance of `alice` to 100, allowing us to execute other transactions. */
+    runtime.balances.set_balance(&String::from("alice"), 100);
+
+    // start emulating a block
+    /* TODO: Increment the block number in system. */
+    runtime.system.inc_block_number();
+    assert_eq!(runtime.system.block_number(), 1);
+    /* TODO: Assert the block number is what we expect. */
+
+    // first transaction
+    /* TODO: Increment the nonce of `alice`. */
+    runtime.system.inc_nonce(&String::from("alice"));
+    /* TODO: Execute a transfer from `alice` to `bob` for 30 tokens.
+        - The transfer _could_ return an error. We should use `map_err` to print
+          the error if there is one.
+        - We should capture the result of the transfer in an unused variable like `_res`.
+    */
+    let _res = runtime
+        .balances
+        .transfer(&String::from("alice"), &String::from("bob"), 30)
+        .map_err(|e| print!("{}", e));
+
+    // second transaction
+    /* TODO: Increment the nonce of `alice` again. */
+    runtime.system.inc_nonce(&String::from("alice"));
+
+    /* TODO: Execute another balance transfer, this time from `alice` to `charlie` for 20. */
+    let _res = runtime
+        .balances
+        .transfer(&String::from("alice"), &String::from("charlie"), 20)
+        .map_err(|e| print!("{}", e));
 }
---
+++ src/system.rs
@@ -0,0 +1,44 @@
+use std::collections::BTreeMap;
+
+/// This is the System Pallet.
+/// It handles low level state needed for your blockchain.
+pub struct Pallet {
+    block_number: u32,
+    nonce: BTreeMap<String, u32>,
+}
+
+impl Pallet {
+    /// Create a new instance of the System Pallet.
+    pub fn new() -> Self {
+        Self { block_number: 0, nonce: BTreeMap::new() }
+    }
+
+    /// Get the current block number.
+    pub fn block_number(&self) -> u32 {
+        self.block_number
+    }
+
+    // This function can be used to increment the block number.
+    // Increases the block number by one.
+    pub fn inc_block_number(&mut self) {
+        self.block_number += 1;
+    }
+
+    // Increment the nonce of an account. This helps us keep track of how many transactions each
+    // account has made.
+    pub fn inc_nonce(&mut self, who: &String) {
+        let user_nonce = *self.nonce.get(who).unwrap_or(&0);
+        self.nonce.insert(who.to_string(), user_nonce + 1);
+    }
+}
+
+#[test]
+fn init_system() {
+    let mut s = Pallet::new();
+
+    s.inc_block_number();
+    assert_eq!(s.block_number(), 1);
+
+    s.inc_nonce(&String::from("wasi"));
+    assert_eq!(s.nonce.get(&String::from("wasi")), Some(1).as_ref());
+}
